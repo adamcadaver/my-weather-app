3c3d7703dbe9d5924341bf4496aafae894f31e80
Adam Labi <labi722@gmail.com>
2020-02-01 19:45:47 -0500

    ran prettier

--- src/App.js
+++ src/App.js
@@ -1,82 +1,87 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const getLocalFavorites = () => {
   const s = window.localStorage;
   s.clear();
-  const favorites = s.getItem('favorites');
+  const favorites = s.getItem("favorites");
   if (favorites === null) {
-    s.setItem('favorites', [])
+    s.setItem("favorites", []);
     return [];
   }
 };
 
-const FavoriteItem = ({value, isSelected, handleClick}) => {
-  let styles = "list-group-item list-group-item-action"
+const FavoriteItem = ({ value, isSelected, handleClick }) => {
+  let styles = "list-group-item list-group-item-action";
   if (isSelected) {
-    styles += " active"
+    styles += " active";
   }
   return (
-    <li className={styles} onClick={() => handleClick(value)}>{value}</li>
+    <li className={styles} onClick={() => handleClick(value)}>
+      {value}
+    </li>
   );
-}
+};
 
-const  FavoritesList = ({items, selected, handleClick}) => {
+const FavoritesList = ({ items, selected, handleClick }) => {
   if (items === undefined || items.size === 0) {
-    return ("No Favorites")
+    return "No Favorites";
   }
   return (
     <ul className="list-group">
-      {Array.from(items).map((zipcode) =>
+      {Array.from(items).map(zipcode => (
         <FavoriteItem
           value={zipcode}
           isSelected={zipcode === selected}
-          handleClick={handleClick}/>
-      )}
+          handleClick={handleClick}
+        />
+      ))}
     </ul>
   );
-}
+};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     // const favorites = getLocalFavorites()
     this.state = {
-     favorites: new Set([10010, 12345, 54353]),
-     selected: undefined,
-     query: ''
+      favorites: new Set([10010, 12345, 54353]),
+      selected: undefined,
+      query: ""
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
-  };
+  }
 
   handleSubmit(event) {
-    this.handleClick(this.state.query)
+    this.handleClick(this.state.query);
     event.preventDefault();
   }
 
   handleClick(zipcode) {
     this.setState({
       selected: zipcode,
-      query: ''
-    })
-}
+      query: ""
+    });
+  }
 
   handleChange(event) {
     this.setState({
       query: event.target.value
-    })
+    });
   }
 
-  render () {
-    const subTitle = this.state.selected ? `Weather for ${this.state.selected}`: '';
+  render() {
+    const subTitle = this.state.selected
+      ? `Weather for ${this.state.selected}`
+      : "";
     return (
       <div className="container">
         <div className="row">
           <h1 className="App-header">My Weather App</h1>
         </div>
-      <div className="row">
+        <div className="row">
           <div className="col-sm-3">
             <div className="row">
               <h2>Favorites</h2>
@@ -90,20 +95,27 @@ class App extends React.Component {
             </div>
           </div>
           <div className="col-sm">
-              <div className="row">
-                <form>
-                  <input type="number" name='zipcodeInput' value={this.state.query} onChange={this.handleChange}/>
-                  <button type="submit" name="submit" onClick={this.handleSubmit}>Submit</button>
-                </form>
-              </div>
-              <div className="row">
-                <h3>{subTitle}</h3>
-              </div>
+            <div className="row">
+              <form>
+                <input
+                  type="number"
+                  name="zipcodeInput"
+                  value={this.state.query}
+                  onChange={this.handleChange}
+                />
+                <button type="submit" name="submit" onClick={this.handleSubmit}>
+                  Submit
+                </button>
+              </form>
+            </div>
+            <div className="row">
+              <h3>{subTitle}</h3>
+            </div>
           </div>
         </div>
       </div>
     );
   }
-};
+}
 
 export default App;
