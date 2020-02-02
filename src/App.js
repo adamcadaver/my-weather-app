aff66f584d1898a69c524469e0652c6985af82b1
Adam Labi <labi722@gmail.com>
2020-02-01 19:42:44 -0500

    setting title

--- src/App.js
+++ src/App.js
@@ -43,24 +43,41 @@ class App extends React.Component {
     // const favorites = getLocalFavorites()
     this.state = {
      favorites: new Set([10010, 12345, 54353]),
-     selected: undefined
+     selected: undefined,
+     query: ''
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   };
+
+  handleSubmit(event) {
+    this.handleClick(this.state.query)
+    event.preventDefault();
+  }
 
   handleClick(zipcode) {
     this.setState({
-      selected: zipcode
+      selected: zipcode,
+      query: ''
+    })
+}
+
+  handleChange(event) {
+    this.setState({
+      query: event.target.value
     })
   }
 
   render () {
+    const subTitle = this.state.selected ? `Weather for ${this.state.selected}`: '';
     return (
       <div className="container">
         <div className="row">
           <h1 className="App-header">My Weather App</h1>
         </div>
-        <div className="row">
-          <div className="col-sm">
+      <div className="row">
+          <div className="col-sm-3">
             <div className="row">
               <h2>Favorites</h2>
             </div>
@@ -68,20 +85,20 @@ class App extends React.Component {
               <FavoritesList
                 items={this.state.favorites}
                 selected={this.state.selected}
-                handleClick={(f) => this.handleClick(f)}
+                handleClick={this.handleClick}
               />
             </div>
           </div>
           <div className="col-sm">
-            <div className="row">
-              <input/>
-            </div>
-            <div className="row">
-              Weather for 10010
-            </div>
-            <div className="row">
-              Sunny
-            </div>
+              <div className="row">
+                <form>
+                  <input type="number" name='zipcodeInput' value={this.state.query} onChange={this.handleChange}/>
+                  <button type="submit" name="submit" onClick={this.handleSubmit}>Submit</button>
+                </form>
+              </div>
+              <div className="row">
+                <h3>{subTitle}</h3>
+              </div>
           </div>
         </div>
       </div>
